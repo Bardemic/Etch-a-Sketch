b70c4b58c0b1801196b437ee58123f704679585b
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-07-01 18:55:48 -0500

    Add RGB Button and Color Picker Functionality

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const container = document.querySelector(".container");
 const sizeSlider = document.querySelector("#sizeSlider");
+const rgbOption = document.querySelector(".rgbOption");
+const colorPicker = document.querySelector(".colorPicker");
 let resolution = 16;
 let containerSize = container.clientWidth;
 let color = "black";
-let randomColor = true;
+let randomColor = false;
 newBoard();
 
 
 function changeColor(drawBox){ 
     let rColor = Math.floor(Math.random()*16777215).toString(16);
-    if(randomColor) {
-        drawBox.style.backgroundColor = `#${rColor}`;
-    }
-    //drawBox.style.backgroundColor = (color);
+    if(randomColor) drawBox.style.backgroundColor = `#${rColor}`;
+    else drawBox.style.backgroundColor = (colorPicker.value);
 }
 
 sizeSlider.oninput = function() {
@@ -21,7 +21,7 @@ sizeSlider.oninput = function() {
     newBoard();
     }
     
-
+rgbOption.addEventListener('click', () => randomColor = !randomColor);
 
 function newBoard(){
     let boxHeight = containerSize / resolution;

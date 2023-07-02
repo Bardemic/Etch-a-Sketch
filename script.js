3c06138f1156ee388aba2ea6ca4b0d5f8839cd6e
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-07-01 19:21:27 -0500

    Add Reset Button and Canvas Slider Functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".canvas");
+const canvasSlider = document.querySelector("#canvasSlider");
 const sizeSlider = document.querySelector("#sizeSlider");
 const rgbOption = document.querySelector(".rgbOption");
 const colorPicker = document.querySelector(".colorPicker");
+const resetButton = document.querySelector(".resetButton");
 let resolution = 16;
 let containerSize = container.clientWidth;
 let color = "black";
@@ -17,13 +19,24 @@ function changeColor(drawBox){
 
 sizeSlider.oninput = function() {
     resolution = this.value;
-    document.querySelectorAll(".drawBox").forEach(box => box.remove());
     newBoard();
-    }
-    
+}
+
+canvasSlider.oninput = function() {
+    console.log(this.value);
+    container.style.width = `${this.value}px`;
+    container.style.height = `${this.value}px`;
+    containerSize = container.clientWidth;
+    newBoard();
+}
+
+
 rgbOption.addEventListener('click', () => randomColor = !randomColor);
 
+resetButton.addEventListener('click', () => newBoard());
+
 function newBoard(){
+    document.querySelectorAll(".drawBox").forEach(box => box.remove());
     let boxHeight = containerSize / resolution;
     for(let i = 0; i < resolution; i++) {
         for(let j = 0; j < resolution; j++) {
